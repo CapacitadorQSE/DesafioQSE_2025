9faa8a96407988751f534cb4a180b30a0dd5eb07
Capacitador <capacitadorqse@gmail.com>
2024-12-24 14:25:54 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,49 +1,39 @@
+// Função para smooth scroll ao clicar nos links do menu
+document.querySelectorAll('.nav-icon').forEach((icon) => {
+  icon.addEventListener('click', (event) => {
+    const targetId = icon.getAttribute('href').substring(1);
+    const targetElement = document.getElementById(targetId);
+    if (targetElement) {
+      event.preventDefault();
+      targetElement.scrollIntoView({ behavior: 'smooth' });
+    }
+  });
+});
+
 // Função para rolagem do carrossel
-document.querySelectorAll(".carousel").forEach((carousel) => {
+document.querySelectorAll('.carousel').forEach((carousel) => {
   let isDown = false;
   let startX;
   let scrollLeft;
 
-  carousel.addEventListener("mousedown", (e) => {
+  carousel.addEventListener('mousedown', (e) => {
     isDown = true;
     startX = e.pageX - carousel.offsetLeft;
     scrollLeft = carousel.scrollLeft;
   });
 
-  carousel.addEventListener("mouseleave", () => {
+  carousel.addEventListener('mouseleave', () => {
     isDown = false;
   });
 
-  carousel.addEventListener("mouseup", () => {
+  carousel.addEventListener('mouseup', () => {
     isDown = false;
   });
 
-  carousel.addEventListener("mousemove", (e) => {
+  carousel.addEventListener('mousemove', (e) => {
     if (!isDown) return;
     const x = e.pageX - carousel.offsetLeft;
     const walk = (x - startX) * 2;
     carousel.scrollLeft = scrollLeft - walk;
-  });
-});
-
-// Navegação no menu inferior
-document.querySelectorAll(".menu-item").forEach((item, index) => {
-  item.addEventListener("click", () => {
-    switch (index) {
-      case 0:
-        window.scrollTo({ top: 0, behavior: "smooth" });
-        break;
-      case 1:
-        document.querySelector(".sustainability").scrollIntoView({ behavior: "smooth" });
-        break;
-      case 2:
-        document.querySelector(".practices").scrollIntoView({ behavior: "smooth" });
-        break;
-      case 3:
-        window.location.href = "https://forms.office.com/r/7j1SaeVL8A?embed=true";
-        break;
-      default:
-        break;
-    }
   });
 });
